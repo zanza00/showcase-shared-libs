444c03413ebf3b9f369c443f8ab7451876927020
Simone Picciani <codefloe@simonepicciani.it>
2025-12-06 00:40:25 +0100

    rename packages for npm publish

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,12 @@ const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "showcase",
+    orgName: "zanza00",
     projectName: "shared-libs",
     webpackConfigEnv,
     argv,
     outputSystemJS: false,
   });
-
-  console.log(defaultConfig);
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
